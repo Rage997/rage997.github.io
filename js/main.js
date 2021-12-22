64bc31e9009ecd0349d4c75579704fcdec9c8e72
Rage997 <zuppin@usi.ch>
2021-12-22 14:50:38 +0100

    Major changes in website. Upgrade to Bootstrap 5

--- js/main.js
+++ js/main.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
         let text = $('#hello-there').text();
         $('#hello-there').text("General Kenobi!");
     })
+
+    $('#mail_btn').click( (e)=>{
+        e.preventDefault();
+        
+        let sender = document.getElementById('name').value;
+        var subject = document.getElementById("subject").value;
+        let body = document.getElementById('query').value;
+
+        console.log(sender, subject, body)
+        window.open(`mailto:test@example.com?subject=${subject + ` from `
+                     + sender}&body=${body}`);
+
+        
+    })
 })
